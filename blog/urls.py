98d8a8da772b3388079eec80bcfd85d8c1103f5c
huang4207100 <1006190286@qq.com>
2018-11-06 16:10:21 +0800

    首页页面添加

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login')
+    path('index', views.index, name='index'),
+    path('login', views.user_login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('<int:blog_id>/', views.get_blog, name="blogs"),
+    path('add_blog',views.add_blog, name='add_blog'),
+    path('<int:blog_id>/edit_blog', views.edit_blog, name='edit_blog'),
+    path('<int:blog_id>/delete_blog', views.delete_blog, name='delete_blog')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse,Http404
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .models import Blog
 # Create your views here.
-def login(request):
-    return JsonResponse({'msg':True})
+
+
+def index(request):
+    return render(request, 'blog/index.html', context=None)
+
+def user_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request,'blog/login.html',context=None)
+    else:
+        return Http404('登录失败,请检查账号和密码')
+
+def user_logout(request):
+    logout(request)
+    return render(request,'blog/logout.html', context=None)
+
+@login_required(login_url='blog/index/')
+def add_blog(request,blog_id):
+    pass 
+
+@login_required(login_url='blog/index/')
+def edit_blog(request,blog_id):
+    pass
+
+@login_required(login_url='blog/index/')
+def delete_blog(request,blog_id):
+    pass
+
+#@login_required(login_url='blog/index/')
+def get_blog(request,blog_id):
+    return render(request, 'blog/blog.html', context=None)
